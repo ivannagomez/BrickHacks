39d3b638c0ad7f14779d848e8e449f68f8e217c1
ivannagomez <57508940+ivannagomez@users.noreply.github.com>
2021-02-21 01:47:12 -0500

    Upload of files

---
+++ playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public CharacterController2D controller;
+    public float runSpeed = 29f;
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        
+    }
+}
